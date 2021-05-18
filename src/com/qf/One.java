e79e6b450841b8536f01a7cc3e2c4cecc9687121
qifei <2572764124@qq.com>
2021-05-18 15:27:37 +0800

    qf002分支

--- src/com/qf/One.java
+++ src/com/qf/One.java
@@ -2,8 +2,6 @@ package com.qf;
 
 public class One {
     public static void main(String[] args) {
-        System.out.println("111");
-        System.out.println("第二次修改");
-        System.out.println("qf001分支上线啦");
+        System.out.println("qf002分支也上线啦！");
     }
 }
